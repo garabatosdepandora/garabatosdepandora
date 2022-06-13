2f7cb0e82c054dbef46ccd8a21f4e3e4500ccead
dennis-adrian <dennisguzmanbo@gmail.com>
2022-06-13 16:35:17 -0400

    Create base template page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import pandorita from "./pandorita.png"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="card">
+        <section className="card__text">
+          <h1>I'm Pandorita</h1>
+          <p>And welcome to my page</p>
+          <p>I hope you enjoy my art!</p>
+        </section>
+
+        <figure className="card__image">
+          <img src={pandorita} alt="pandorita" />
+        </figure>
+      </section>
     </div>
   );
 }
